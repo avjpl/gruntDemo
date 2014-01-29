eeb01a800c2d7237311da421b2d57203143da896
Adrian <avjpl@aol.com>
2014-01-29 09:16:25 +0000

    removed sass from task array

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,32 +14,32 @@ module.exports = function(grunt) {
         }
     },
     uglify: {
-        options: {
-            banner: '/*! <%= pkg.name %> - v<%= pkg.version %> */\n',
+      options: {
+//      banner: '/*! <%= pkg.name %> - v<%= pkg.version %> */\n',
         mangle: false
-        },
-        dist: {
-            files: {
-                'build/js/app.min.js': ['<%= concat.dist.dest %>']
-            }
+      },
+      dist: {
+        files: {
+          'build/js/app.min.js': ['<%= concat.dist.dest %>']
+          }
         }
     },
     jshint: {
-        options: {
-            jshintrc: true
-        },
-        files: {
-            src: ['src/**/*.js']
-        }
+      options: {
+        jshintrc: true
+      },
+      files: {
+        src: ['src/**/*.js']
+      }
     },
     jasmine: {
-        pivotal: {
-            src: 'src/**/*.js',
-            options: {
-                specs: 'tests/**/specs/*Spec.js',
-                helpers: 'tests/**/specs/*Helper.js'
-            }
+      pivotal: {
+        src: 'src/**/*.js',
+        options: {
+          specs: 'tests/**/specs/*Spec.js',
+          helpers: 'tests/**/specs/*Helper.js'
         }
+      }
     },
     sass: {
       dist: {
@@ -54,19 +54,28 @@ module.exports = function(grunt) {
           'build/css/main.min.css': ['build/css/main.css']
         }
       }
+    },
+    useminPrepare: {
+      html: 'index.htm',
+      options: {
+          dest: 'build'
+      }
+    },
+    usemin: {
+      html: ['build/{,*/}*.htm'],
+      css: ['build/css/{,*/}*.css']
     }
   });
 
-  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-usemin');
 
   // Build task(s).
-  grunt.registerTask('default', ['jshint', 'clean:build',  'concat', 'uglify', 'sass', 'cssmin']);
-  grunt.registerTask('test', ['default', 'jasmine']);
+  grunt.registerTask('default', ['clean:build', 'useminPrepare', 'jshint', 'concat', 'uglify', 'cssmin', 'usemin']);
 };
